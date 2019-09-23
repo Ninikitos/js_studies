5917c943c1baab8555c3b383a00bc27009629c53
Nick <nilin@magicleap.com>
2019-09-23 13:15:34 -0400

    Useful console functions

---
+++ js_sandbox/main.js
@@ -0,0 +1,33 @@
+// Useful console output
+
+console.log('Hello');
+console.log(123);
+console.log(true);
+var greeting = "Hello var";
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+console.log({a:1, b:2})
+console.table({a:1, b:2});
+
+console.error('This is some error');
+console.clear();
+console.warn('This is warning');
+console.time('Hello');
+    console.log(123);
+    console.log(123);
+    console.log(123);
+    console.log(123);
+    console.log(123);
+    console.log(123);
+    console.log(123);
+    console.log(123);
+    console.log(123);
+console.timeEnd('Hello');
+
+
+
+/*
+multi
+line
+comment
+*/
