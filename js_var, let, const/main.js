40fd6477f57c20f88f9bc1f3d424c9eab574e9e8
Nick <nilin@magicleap.com>
2019-09-23 13:55:13 -0400

    var, let, const

---
+++ js_var, let, const/main.js
@@ -0,0 +1,64 @@
+// var
+
+// var name = 'Nick Ilin';
+// console.log(name);
+
+// name = 'Mykyta Ilin';
+// console.log(name);
+
+// // init var
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+// // can include letters, numbers, _, $
+// // cannot start with number
+// // _name - privat var
+
+// // Multi word var
+// var firstName = 'Nick';
+// var second_name = 'Mykyta';
+// var ThirdName = 'John';
+
+// -----------------------------------------------------------------------------
+
+// let
+
+// let name = 'Nick Ilin';
+// console.log(name);
+
+// name = 'Mykyta Ilin';
+// console.log(name);
+
+// // init var
+// let greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+// -----------------------------------------------------------------------------
+
+//const
+
+// const name = 'Nick Ilin';
+// console.log(name);
+
+// name = 'Mykyta Ilin';
+// console.log(name);
+
+// Always needs to have a value
+// const lastName;
+// console.log(lastName);
+
+// const person = {
+//     name: 'Nick',
+//     age: 29
+// }
+
+// person.name = 'Sara';
+
+// console.log(person.name);
+
+
+
