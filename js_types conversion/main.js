94f5e7f0f9604441f2f5cb2f83aa262ad3d5ec2b
Nick <nilin@magicleap.com>
2019-09-23 15:16:29 -0400

    type converstions

---
+++ js_types conversion/main.js
@@ -0,0 +1,35 @@
+let val;
+
+//Number to string
+val = String(5);
+val = String(4+4);
+
+//Bool to string
+val = String(true);
+
+//Date to string
+val = String(new Date());
+
+//Array to string
+val = String([1, 2, 3]);
+
+//toString()
+val = (5).toString();
+val = (true).toString();
+
+// String to number
+val = Number('10');
+val = Number(null);
+val = Number('Hi');
+
+//Output
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.length);
+// console.log(val.toFixed(2));
+
+let val1 = String(6);
+let val2 = 10;
+let sum = Number(val1 + val2);
+
+console.log(sum);
