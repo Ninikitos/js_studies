2368788013b6777563a790155022250ff89f92e8
Nick <nilin@magicleap.com>
2019-09-23 14:38:46 -0400

    data types

---
+++ js_data types/main.js
@@ -0,0 +1,41 @@
+// Primitive
+
+// string
+const name = 'Nick';
+
+// number
+const age = 29;
+
+// bool
+const isBool = true;
+
+// null
+const car = null;
+
+// undefined
+let test;
+
+// symbol
+const sym = Symbol();
+
+
+console.log(typeof sym); 
+
+//-----------------------------------------------------------------------
+
+// Reference
+
+// array
+const array = [1, 1, 1, 2, 2, 3];
+
+//Object literal
+const address = {
+    city: 'Lugansk', 
+    country: 'Ukraine'
+}
+
+const today = new Date();
+
+
+console.log(today);
+console.log(typeof address);
